48305a93879232d945cf67c46c8ef437a6c27dc1
Sabbadini10 <87876202+Sabbadini10@users.noreply.github.com>
2021-10-14 10:31:34 -0300

    Update formulario.js

--- formulario.js
+++ formulario.js
@@ -1,39 +1,3 @@
-function validar() {
-    var nombre, apeliidos, correo, usuario, clave, telefono, expresion;
-    nombre = document.getElementById("nombre").value;
-    apeliidos = document.getElementById("apellidos").value;
-    email = document.getElementById("email").value;
-    fecha = document.getElementById("fechaNacimiento").value;
-    textarea = document.getElementById("input-textarea").value;
-    expresion = /\w+@\w+\.+[a-z]/;
-
-    if (nombre == "" || apeliido == "" || email == "" || fecha == "" || textarea == "") {
-
-        alert("Todos los campos son obligatorios");
-        return false;
-    }
-    if (nombre.length > 15) {
-        alert("El nombre es muy largo");
-        return false;
-    }
-    if (apeliido.length > 20) {
-        alert("Los apellido son muy largos ");
-        return false;
-    }
-    if (email.length > 30) {
-        alert("El correo es muy largo");
-        return false;
-    }
-    if (!expresion.test(email)) {
-        alert("el correo no es valido ");
-        return false;
-    }
-    if (textarea.length > 200) {
-        alert("El mensaje solo debe tener 200 caracteres como maximo");
-        return false;
-    }
-}
-
 $(function () {
     $.datepicker.setDefaults($.datepicker.regional["es"]);
     $("#fechaNacimiento").datepicker({
